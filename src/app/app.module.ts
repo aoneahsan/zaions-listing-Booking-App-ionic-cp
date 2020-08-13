d2d99482346e502012f472dd29f5fabe65187ce3
ahsanmahmood <aoneahsan@gmail.com>
2020-08-13 14:58:53 +0500

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,6 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,7 +17,7 @@ import { environment } from '../environments/environment';
     BrowserModule, 
     HttpClientModule,
     IonicModule.forRoot(), 
-    AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    AppRoutingModule
   ],
   providers: [
     StatusBar,
